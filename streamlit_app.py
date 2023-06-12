511f06d5c5ff00eea24b786d0673dff674ae171b
Obroo <35345663+Obroo@users.noreply.github.com>
2023-06-12 10:20:53 +0200

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 
 import streamlit
 
-streamlit.title('Hello everyone')
+streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menun')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
